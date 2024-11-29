b110b70a5aefd0eddbe5c91c173654291448272a
RAP <127470011+Moksennn@users.noreply.github.com>
2024-11-30 03:41:37 +0900

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ https://huggingface.co/Lord769/Models/resolve/main/NUKEPonyPixarStyle_v2S.safete
 https://huggingface.co/Lord769/Models/resolve/main/AnotherMixPXL_v31.safetensors
 
 #Lora
-https://civitai.com/api/download/models/406776
+https://huggingface.co/Lord769/Models/resolve/main/Sylpha.safetensors
 
 #VAE
 https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors
