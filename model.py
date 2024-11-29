d43edc20e07dd667b8d91c7c88cc0d03814671e2
RAP <127470011+Moksennn@users.noreply.github.com>
2024-11-30 03:01:11 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,9 @@
 #Model
-https://civitai.com/api/download/models/162101
+https://huggingface.co/Lord769/Models/resolve/main/NUKEPonyPixarStyle_v2S.safetensors
+https://huggingface.co/Lord769/Models/resolve/main/AnotherMixPXL_v31.safetensors
 
 #Lora
-https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.safetensors
-https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.png
-https://civitai.com/api/download/models/135867
+https://civitai.com/api/download/models/406776
 
 #VAE
-https://civitai.com/api/download/models/164360?type=VAE
-https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/main/vae/kl-f8-anime2.ckpt
-
-#ControlNet
-https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny_fp16.safetensors
+https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors
