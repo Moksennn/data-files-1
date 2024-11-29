f42eb30a5155ba1c1e7921714f1766b86b2c17c1
RAP <127470011+Moksennn@users.noreply.github.com>
2024-11-30 02:56:02 +0900

    Add files via upload

---
+++ model.py
@@ -0,0 +1,14 @@
+#Model
+https://civitai.com/api/download/models/162101
+
+#Lora
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.png
+https://civitai.com/api/download/models/135867
+
+#VAE
+https://civitai.com/api/download/models/164360?type=VAE
+https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/main/vae/kl-f8-anime2.ckpt
+
+#ControlNet
+https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny_fp16.safetensors
