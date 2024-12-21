7f0a3772885ef0555578d6b21fbf30e914fe7c26
RAP <127470011+Moksennn@users.noreply.github.com>
2024-12-21 22:05:02 +0900

    Update and rename model (2).py to model.py

---
+++ model.py
@@ -0,0 +1,9 @@
+#Model
+https://huggingface.co/Lord769/Models/resolve/main/evaclausmixPonyXL_v35.safetensors
+https://huggingface.co/Lord769/Models/resolve/main/miaomiaoHarem_v13.safetensors
+
+#Lora
+https://huggingface.co/Lord769/Models/resolve/main/Sylpha.safetensors
+
+#VAE
+https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors
