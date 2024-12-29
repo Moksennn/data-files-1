c90fa3b3cc7aa25dbd18f198e421489bb1030332
RAP <127470011+Moksennn@users.noreply.github.com>
2024-12-29 12:46:48 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 #Model
-https://huggingface.co/Lord769/Models/resolve/main/evaclausmixPonyXL_v35.safetensors
+https://huggingface.co/Lord769/Models/resolve/main/AnotherMixPXL_v31.safetensors
 https://huggingface.co/Lord769/Models/resolve/main/miaomiaoHarem_v13.safetensors
 
 #Lora
