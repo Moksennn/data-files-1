785065922100ef0c5361d03722bdcd34f2764e57
RAP <127470011+Moksennn@users.noreply.github.com>
2025-01-05 11:58:02 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 #Model
 https://huggingface.co/Lord769/Models/resolve/main/AnotherMixPXL_v31.safetensors
-https://huggingface.co/Lord769/Models/resolve/main/fnMixPonyXL_v16.safetensors
+https://huggingface.co/Lord769/Models/resolve/main/miaomiaoHarem_v13.safetensors
 
 #Lora
 https://huggingface.co/Lord769/Models/resolve/main/Sylpha.safetensors
