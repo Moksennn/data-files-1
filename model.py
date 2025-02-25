6ca68113a32727fa7825b850283fb625a048f225
RAP <127470011+Moksennn@users.noreply.github.com>
2025-02-25 21:38:15 +0900

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,22 @@ https://huggingface.co/Lord769/Models/resolve/main/AniIllustrioussxlv2.safetenso
 https://huggingface.co/Lord769/Models/resolve/main/prefectIllustriousXL_v10.safetensors
 
 #Lora
-https://huggingface.co/Lord769/Models/resolve/main/Sylpha.safetensors
+https://huggingface.co/Lord769/Models/resolve/main/AmagiBrilliantPark_SentoIsuzu_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/HighSchoolDxDHERO_AsiaArgento_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/HighSchoolDxDHERO_HimejimaAkeno_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/HighSchoolDxDHERO_PhenexRavel_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/HighSchoolDxDHERO_RiasGremory_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/HighSchoolDxDHERO_Rossweisse_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/HighSchoolDxDHERO_ShidouIrina_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/HighSchoolDxDHERO_Yaasaka_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/Kakegurui_JabamiYumeko_IlluXL.safetensors?download=true
+https://huggingface.co/Lord769/Models/resolve/main/MahoukaKoukouNoRettousei_AngelinaKudouShields_IlluXL.safetensors?download=true
+
+
+
+
+
+
 
 #VAE
 https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors
