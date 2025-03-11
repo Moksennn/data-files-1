06a9cac577f177bb5268e7ef66d210169db3cb16
RAP <127470011+Moksennn@users.noreply.github.com>
2025-03-11 10:50:53 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 #Model
-https://huggingface.co/Lord769/Models/resolve/main/AniIllustrioussxlv2.safetensors
-https://huggingface.co/Lord769/Models/resolve/main/prefectIllustriousXL_v10.safetensors
+https://huggingface.co/Lord769/Models/resolve/main/miaomiaoHarem_baseV10.safetensors
 
 #Lora
 https://huggingface.co/Lord769/Models/resolve/main/AmagiBrilliantPark_SentoIsuzu_IlluXL.safetensors
