1446423b09e46a9933eba691fa56adabc631deae
RAP <127470011+Moksennn@users.noreply.github.com>
2025-04-04 09:06:14 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 #Model
-https://huggingface.co/Lord769/Models/resolve/main/aniillustriousxl_v30.safetensors
 https://huggingface.co/Lord769/Models/resolve/main/cocoIllustriousNoobai_2dV10.safetensors
 
 #Lora
