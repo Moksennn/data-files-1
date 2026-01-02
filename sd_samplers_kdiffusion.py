8858d3b4fe20eea9f5a7791f5c28dc0e2a8e2755
RAP <127470011+Moksennn@users.noreply.github.com>
2026-01-02 16:59:58 +0900

    Update sd_samplers_kdiffusion.py

--- sd_samplers_kdiffusion.py
+++ sd_samplers_kdiffusion.py
@@ -17,8 +17,8 @@ samplers_k_diffusion = [
     ('DPM++ 3M SDE', 'sample_dpmpp_3m_sde', ['k_dpmpp_3m_sde'], {'scheduler': 'exponential', 'discard_next_to_last_sigma': True, "brownian_noise": True}),
     ('Euler a', 'sample_euler_ancestral', ['k_euler_a', 'k_euler_ancestral'], {"uses_ensd": True}),
     ('Euler', 'sample_euler', ['k_euler'], {}),
-    ("Euler Dy", sample_euler_dy, ["k_euler_dy"], {}),
-    ("Euler SMEA Dy", sample_euler_smea_dy, ["k_euler_smea_dy"], {}),
+    ('Euler Dy', 'sample_euler_dy', ['k_euler_dy'], {}),
+    ('Euler SMEA Dy', 'sample_euler_smea_dy', ['k_euler_smea_dy'], {}),
     ('LMS', 'sample_lms', ['k_lms'], {}),
     ('Heun', 'sample_heun', ['k_heun'], {"second_order": True}),
     ('DPM2', 'sample_dpm_2', ['k_dpm_2'], {'scheduler': 'karras', 'discard_next_to_last_sigma': True, "second_order": True}),
@@ -234,6 +234,7 @@ class KDiffusionSampler(sd_samplers_common.Sampler):
         self.add_infotext(p)
 
         return samples
+
 
 
 
